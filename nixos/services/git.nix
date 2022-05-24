6b7c90b006803a2ec1ca4c75e6fc86622cbaf9c2
Yury Shvedov <Yury.Shvedov@kaspersky.com>
2022-05-24 12:19:45 +0300

    [git] Add more options
    Change-Id: I90881a60d45375bb64da9df38d16f90d170c5636

--- nixos/services/git.nix
+++ nixos/services/git.nix
@@ -4,6 +4,7 @@
     local.extras = (pkgs.mkLocalExtra "programs" {
       git = lib.mkIf (config.local.user.mail != null) {
         enable = true;
+        lfs.enable = true;
         package = pkgs.gitFull;
         userName = config.local.user.name;
         userEmail = config.local.user.mail;
@@ -18,10 +19,17 @@
             diff = {
               colorMoved = "zebra";
             };
+            http = {
+              version = "HTTP/1.1";
+            };
           };
         }];
       };
     });
+    programs.git = {
+      enable = true;
+      lfs.enable = true;
+    };
     environment.systemPackages = [
       pkgs.git-review
     ];
