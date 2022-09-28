1213aa318063aa6213b5cba0fac364e3017513c0
Yury Shvedov <mestofel13@gmail.com>
2022-09-28 12:18:34 +0300

    [serivces/git] Add gitupdate script
    This script will rebase you last branch on your HEAD without first
    commit. Use cases:
    
        $ git checkout HEAD~2 # go to commit to change
        $ vim # edit code
        $ git commit -a --amend --no-edit # create new commit
        $ gitupdate # replace commit with new version in previous branch
        $ #--------
        $ git checkout HEAD~3 # go to another commit
        $ git stash pop # load changes from stash
        $ gcmnu # update commit and replace old commit in branch
        $ #--------
        $ git checkout HEAD^ # go to previous commit
        $ vim # edit code
        $ gcmu -a # update commit with message and replace old commit

--- nixos/services/git.nix
+++ nixos/services/git.nix
@@ -1,4 +1,36 @@
 { config, pkgs, lib, ... }:
+let
+  git = "${pkgs.git}/bin/git";
+  gitupdate = pkgs.writeShellScriptBin "gitupdate" ''
+    set -e
+
+    PREV="''${1-"@{-1}"}"
+    BRANCH="$(${git} rev-parse --abbrev-ref @{-1})"
+    [ -z "$BRANCH" ] && BRANCH="$PREV"
+    [ -z "$BRANCH" ] && echo "Invalid revision $PREV" && exit 1
+    START="$(${git} log --reverse --ancestry-path --pretty=format:"%h" \
+      --no-patch "HEAD^..$BRANCH" | head -1)"
+
+    exec ${git} rebase --onto HEAD $START $BRANCH
+  '';
+  gcme = noedit: let
+    name = if noedit then "gcmnu" else "gcmu";
+    op = if noedit then "--no-edit" else "";
+  in pkgs.writeShellScriptBin name ''
+    set -e
+    ${git} commit --amend ${op} "$@"
+    DOPOP="true"
+    if [ "$(${git} diff --stat)" != "" ]; then
+      DOPOP="${git} stash pop"
+      ${git} stash
+    fi
+    ${gitupdate}/bin/gitupdate
+    $DOPOP
+  '';
+  gcmnu = gcme true;
+  gcmu = gcme false;
+
+in
 {
   config = {
     local.extras = (pkgs.mkLocalExtra "programs" {
@@ -32,6 +64,9 @@
     };
     environment.systemPackages = [
       pkgs.git-review
+      gitupdate
+      gcmnu
+      gcmu
     ];
   };
 }
