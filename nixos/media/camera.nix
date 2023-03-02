41a4b78e78a030fdddbe0cc08641f4468813dd94
Yury Shvedov <Yury.Shvedov@kaspersky.com>
2023-03-02 11:19:48 +0300

    [media] Set absolute focus for MS HD Cam
    Change-Id: If775f1b6cf81dc5d93ab4041e53586f0adc7f277

--- nixos/media/camera.nix
+++ nixos/media/camera.nix
@@ -7,7 +7,7 @@
         ATTRS{idVendor}=="045e", \
         ATTRS{idProduct}=="076d", \
         GROUP="dialout", \
-        RUN+="${pkgs.v4l-utils}/bin/v4l2-ctl -c focus_auto=0"
+        RUN+="${pkgs.v4l-utils}/bin/v4l2-ctl -c focus_auto=0 -c focus_absolute=10"
       ''
     ];
   };
