c1f25ea05d4020a14a7bb6b7cdf119db38768d6e
Yury Shvedov <yury.shvedov@kaspersky.com>
2023-08-03 21:03:37 +0300

    [networking] Move firewall config to flakes
    Change-Id: Ib0f8b82c55bbe827f099b9bf34700b2dbae686f0

--- nixos/modules/config/default.nix
+++ nixos/modules/config/default.nix
@@ -7,5 +7,6 @@
     ./desktop
     ./media
     ./tools
+    ./services
   ];
 }
--- nixos/modules/config/services/networking/firewall.nix
+++ nixos/modules/config/services/networking/firewall.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, ... }:
+{ ... }:
 {
   networking.firewall.trustedInterfaces = [
     #Allow our containers to interact with host
