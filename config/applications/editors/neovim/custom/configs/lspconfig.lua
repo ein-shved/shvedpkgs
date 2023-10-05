0e9b60f3f589e1ca72eea1f7a5c33e2fade9ccaa
Yury Shvedov <yury.shvedov@kaspersky.com>
2023-10-05 14:55:44 +0300

    [neovim] Refactor lspconfig
    To fix basic finctionality initialisation for some lsp servers
    
    Change-Id: Ie87db9110df62898d40645386f5fefc54974d121

--- config/applications/editors/neovim/custom/configs/lspconfig.lua
+++ config/applications/editors/neovim/custom/configs/lspconfig.lua
@@ -1,79 +1,81 @@
-local lspconfig = require "lspconfig"
+local lspconfig = require("lspconfig")
 
 -- if you just want default config for the servers then put them in a table
 local servers = {
-  "clangd",
-  "lua_ls",
   "rnix",
-  "rust_analyzer",
   "bashls",
   "cmake",
-}
-
-local on_attach = require("plugins.configs.lspconfig").on_attach
-
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = function(client, bufnr)
-      -- Presave and restore formating capabilities of servers after NvChad
-      -- changes them
-      local formattingProvider =
-          client.server_capabilities.documentFormattingProvider;
-      local rangeFormattingProvider =
-          client.server_capabilities.documentRangeFormattingProvider;
-
-      on_attach(client, bufnr)
-
-      client.server_capabilities.documentFormattingProvider = formattingProvider;
-      client.server_capabilities.documentRangeFormattingProvider = rangeFormattingProvider;
-    end,
-
-    capabilities = require("plugins.configs.lspconfig").capabilities,
-  }
-end
-
+  lua_ls = {
+    settings = {
 -- Took from https://github.com/neovim/neovim/issues/21686
-lspconfig.lua_ls.setup {
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using
-        -- (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {
-          'vim',
-          'require'
+      Lua = {
+        runtime = {
+          -- Tell the language server which version of Lua you're using
+          -- (most likely LuaJIT in the case of Neovim)
+          version = "LuaJIT",
+        },
+        diagnostics = {
+          -- Get the language server to recognize the `vim` global
+          globals = {
+            "vim",
+            "require",
+          },
+        },
+        workspace = {
+          -- Make the server aware of Neovim runtime files
+          library = vim.api.nvim_get_runtime_file("", true),
+          checkThirdParty = false,
+        },
+        -- Do not send telemetry data containing a randomized but unique identifier
+        telemetry = {
+          enable = false,
         },
       },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-        checkThirdParty = false,
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
+    },
+  },
+  clangd = {
+    cmd = {
+      "clangd",
+      "-j=8",
+      "--query-driver=/home/shved/kl/**/*,/nix/store/**/*,*",
+      "--header-insertion=never",
+    },
+  },
+  rust_analyzer = {
+    settings = {
+      ["rust-analyzer"] = {
+        diagnostics = {
+          enable = true,
+        },
       },
     },
   },
 }
 
-lspconfig.clangd.setup {
-  cmd = { 'clangd', '-j=8',
-    '--query-driver=/home/shved/kl/**/*,/nix/store/**/*,*',
-    '--header-insertion=never'
-  },
-}
+local on_attach_orig = require("plugins.configs.lspconfig").on_attach
+local function on_attach(client, bufnr)
+  -- Presave and restore formating capabilities of servers after NvChad
+  -- changes them
+  local formattingProvider = client.server_capabilities.documentFormattingProvider
+  local rangeFormattingProvider = client.server_capabilities.documentRangeFormattingProvider
 
-lspconfig.rust_analyzer.setup {
-  settings = {
-    ['rust-analyzer'] = {
-      diagnostics = {
-        enable = true,
-      }
-    }
+  on_attach_orig(client, bufnr)
+
+  client.server_capabilities.documentFormattingProvider = formattingProvider
+  client.server_capabilities.documentRangeFormattingProvider = rangeFormattingProvider
+end
+
+for lsp, val in pairs(servers) do
+  local setup = {
+    on_attach = on_attach,
+    capabilities = require("plugins.configs.lspconfig").capabilities,
   }
-}
+  if type(lsp) == "number" and type(val) == "string" then
+    lsp = val
+    val = nil
+  end
+  if val ~= nil then
+    setup = vim.tbl_deep_extend("force", setup, val)
+  end
+  lspconfig[lsp].setup(setup)
+end
