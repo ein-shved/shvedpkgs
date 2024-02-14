483176e1187f282d0971669ea6126551178275c2
Yury Shvedov <yury.shvedov@kaspersky.com>
2024-02-14 14:45:12 +0300

    [services/ssh] Disable masterSocket feature
    Change-Id: I6eb2ef88ca5f2630ac1c3b662826a0f91839de3a

--- config/services/networking/ssh.nix
+++ config/services/networking/ssh.nix
@@ -12,17 +12,9 @@
       extras = (lib.mkHmExtra "programs" {
         ssh = {
           enable = true;
-          controlMaster = "yes";
-          controlPath = "~/.ssh/control/master-%r@%n:%p";
-          controlPersist = "30m";
           serverAliveInterval = 10;
         };
       });
-      activations = {
-        createSshControls = ''
-          mkdir ~/.ssh/control -p;
-        '';
-      };
     };
   };
 }
