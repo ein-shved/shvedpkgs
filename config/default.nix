9dab09eafcd230eaa3658bdd9ab6f765f87bff6d
Yury Shvedov <yury.shvedov@kaspersky.com>
2024-07-02 22:23:06 +0300

    [config/nix] Add hyprland cachix substituter

--- config/default.nix
+++ config/default.nix
@@ -33,6 +33,8 @@
     nix.settings = {
       auto-optimise-store = true;
       experimental-features = [ "nix-command" "flakes" ];
+      substituters = ["https://hyprland.cachix.org"];
+      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
     };
     documentation.nixos = {
       includeAllModules = true;
