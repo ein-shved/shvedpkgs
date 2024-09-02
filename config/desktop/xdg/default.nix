9fa0ecc45517cf61b09ce4376fca2231eb96f78f
Yury Shvedov <yury.shvedov@kaspersky.com>
2024-09-02 11:02:43 +0300

    Enable xdg-portal and upower
    Change-Id: Ib6423af2df439a08959565d1feb8c9502f549abb

--- config/desktop/xdg/default.nix
+++ config/desktop/xdg/default.nix
@@ -1,6 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 {
   imports = [
     ./mime
   ];
+  config = {
+    xdg.portal.enable = true;
+    xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
 }
--- config/services/default.nix
+++ config/services/default.nix
@@ -6,4 +6,5 @@
   ];
   services.power-profiles-daemon.enable = true;
   services.udisks2.enable = true;
+  services.upower.enable = true;
 }
