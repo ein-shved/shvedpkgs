26e1aa8eee65032684790b4ec7412972047e31ec
Yury Shvedov <yury.shvedov@kaspersky.com>
2024-10-02 18:25:01 +0300

    [config/git] add lg alias
    Change-Id: I2957de9f7a952dbdb1705895af20332cd54d9a77

--- config/applications/version-management/git/default.nix
+++ config/applications/version-management/git/default.nix
@@ -36,6 +36,9 @@ in
             core = {
               excludesFile = "${gitignore}";
             };
+            alias = {
+              lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+            };
           };
         }];
       };
