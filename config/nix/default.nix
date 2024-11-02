aaf44cce8f7f3aab577eccae4a97a590ebe54050
Yury Shvedov <yury.shvedov@kaspersky.com>
2024-11-02 10:58:57 +0300

    [nix] Auto-generate and apply private key
    Change-Id: If05120f576c68ac6e6316f0da21f4150718124d7

--- config/nix/default.nix
+++ config/nix/default.nix
@@ -1,4 +1,4 @@
-{ config, lib, system, ... }:
+{ config, lib, system, pkgs, ... }:
 let
   domainHost = config.kl.domain.host;
   userName = config.user.name;
@@ -6,6 +6,16 @@ let
   domainUrl = "ssh://${userName}@${domainHost}";
 in
 {
+  systemd.services.generate-nix-cache-key = {
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig.Type = "oneshot";
+    path = [ pkgs.nix ];
+    script = ''
+      [[ -f /etc/nix/private-key ]] && exit
+      nix-store --generate-binary-cache-key ${config.networking.hostName}-1 \
+        /etc/nix/private-key /etc/nix/public-key
+    '';
+  };
   nix = {
     sshServe.enable = true;
     settings = {
@@ -15,6 +25,7 @@ in
         (lib.lists.optional config.kl.remote.enable domainUrl);
       trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
       trusted-users = [ config.user.name ];
+      secret-key-files = "/etc/nix/private-key";
     };
     distributedBuilds = true;
     buildMachines = lib.lists.optional config.kl.remote.enable {
