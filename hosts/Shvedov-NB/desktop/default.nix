fac186fba58d0afd32ded0d1545ed35de4eaf0c4
Yury Shvedov <yury.shvedov@kaspersky.com>
2024-12-02 12:08:46 +0300

    [desktop/hyprland] Fix configuration after upgrade

--- hosts/Shvedov-NB/desktop/default.nix
+++ hosts/Shvedov-NB/desktop/default.nix
@@ -1,20 +1,26 @@
 {
-  programs.hyprland.monitors = {
-    eDP-1 = {
-      resolution = "1920x1080";
-      position = "0x0";
-      scale = "1";
-      wallpaper = ./wallpapers/Demeter.jpg;
+  programs.hyprland.monitors =
+    let
+      ultrawide = {
+        resolution = "3440x1440";
+        position = "-760x1080";
+        scale = "1";
+        wallpaper = ./wallpapers/OuterWilds.png;
+      };
+    in
+    {
+      eDP-1 = {
+        resolution = "1920x1080";
+        position = "0x0";
+        scale = "1";
+        wallpaper = ./wallpapers/Demeter.jpg;
+      };
+      default = {
+        position = "auto-right";
+        wallpaper = ./wallpapers/RoR2.jpg;
+      };
+      # Found out that description of this monitor changes
+      "desc:XMI Mi Monitor 0000000000000" = ultrawide;
+      "desc:Xiaomi Corporation Mi Monitor 0000000000000" = ultrawide;
     };
-    "desc:XMI Mi Monitor 0000000000000" = {
-      resolution = "3440x1440";
-      position = "-760x1080";
-      scale = "1";
-      wallpaper = ./wallpapers/OuterWilds.png;
-    };
-    default = {
-      position = "auto-right";
-      wallpaper = ./wallpapers/RoR2.jpg;
-    };
-  };
 }
