f0dc869648f7dffd52f144a0fab0f5e961f00d1d
Yury Shvedov <yury.shvedov@kaspersky.com>
2025-01-31 11:19:24 +0300

    xdg: explicitly configure portals for niri
    Change-Id: I7eb388fc27dd9e82dcd636a8f4ba11911389aca3

--- config/desktop/xdg/default.nix
+++ config/desktop/xdg/default.nix
@@ -5,6 +5,64 @@
   ];
   config = {
     xdg.portal.enable = true;
-    xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    xdg.portal.extraPortals = with pkgs; [
+      xdg-desktop-portal-wlr
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-gnome
+    ];
+    xdg.portal.config = {
+      niri = {
+        default = [
+          "wlr"
+          "gtk"
+          "gnome"
+        ];
+
+        # Source: https://gitlab.archlinux.org/archlinux/packaging/packages/sway/-/commit/87acbcfcc8ea6a75e69ba7b0c976108d8e54855b
+        "org.freedesktop.impl.portal.Inhibit" = "none";
+
+        # Disable keyring
+        "org.freedesktop.impl.portal.Secret" = "none";
+
+        # wlr interfaces
+        # "org.freedesktop.impl.portal.ScreenCast" = "wlr";
+        # "org.freedesktop.impl.portal.Screenshot" = "wlr";
+
+        # gnome-keyring interfaces
+        # "org.freedesktop.impl.portal.Secret" = "gnome-keyring";
+
+        # GTK interfaces
+        "org.freedesktop.impl.portal.FileChooser" = "gtk";
+        "org.freedesktop.impl.portal.AppChooser" = "gtk";
+        "org.freedesktop.impl.portal.Print" = "gtk";
+        "org.freedesktop.impl.portal.Notification" = "gtk";
+        # "org.freedesktop.impl.portal.Inhibit" = "gtk";
+        "org.freedesktop.impl.portal.Access" = "gtk";
+        "org.freedesktop.impl.portal.Account" = "gtk";
+        "org.freedesktop.impl.portal.Email" = "gtk";
+        "org.freedesktop.impl.portal.DynamicLauncher" = "gtk";
+        "org.freedesktop.impl.portal.Lockdown" = "gtk";
+        "org.freedesktop.impl.portal.Settings" = "gtk";
+        "org.freedesktop.impl.portal.Wallpaper" = "gtk";
+
+        # Gnome interfaces
+        # "org.freedesktop.impl.portal.Access" = "gnome";
+        # "org.freedesktop.impl.portal.Account" = "gnome";
+        # "org.freedesktop.impl.portal.AppChooser" = "gnome";
+        "org.freedesktop.impl.portal.Background" = "gnome";
+        "org.freedesktop.impl.portal.Clipboard" = "gnome";
+        # "org.freedesktop.impl.portal.DynamicLauncher" = "gnome";
+        # "org.freedesktop.impl.portal.FileChooser" = "gnome";
+        "org.freedesktop.impl.portal.InputCapture" = "gnome";
+        # "org.freedesktop.impl.portal.Lockdown" = "gnome";
+        # "org.freedesktop.impl.portal.Notification" = "gnome";
+        # "org.freedesktop.impl.portal.Print" = "gnome";
+        "org.freedesktop.impl.portal.RemoteDesktop" = "gnome";
+        "org.freedesktop.impl.portal.ScreenCast" = "gnome";
+        "org.freedesktop.impl.portal.Screenshot" = "gnome";
+        # "org.freedesktop.impl.portal.Settings" = "gnome";
+        # "org.freedesktop.impl.portal.Wallpaper" = "gnome";
+      };
+    };
   };
 }
