5a21494679484ddcd38bc21102e0c2bd7726a42e
Yury Shvedov <mestofel13@gmail.com>
2025-04-12 15:27:27 +0300

    ShvedMedia: setup media folder

--- hosts/ShvedMedia/configuration.nix
+++ hosts/ShvedMedia/configuration.nix
@@ -46,6 +46,8 @@ in
     ];
   };
 
+  services.mediaStore = "/media";
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
