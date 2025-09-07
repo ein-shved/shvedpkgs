edd8ea7b3f59571a971c8dba0819d31ddfe57870
Yury Shvedov <mestofel13@gmail.com>
2025-09-07 12:18:08 +0300

    upnp: Switching from jellyfin to minidlna

--- config/services/nas/upnp/default.nix
+++ config/services/nas/upnp/default.nix
@@ -1,18 +1,29 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }:
 {
-  services.jellyfin = lib.mkIf config.hardware.isNas {
+  services.minidlna = lib.mkIf config.hardware.isNas {
     enable = true;
     openFirewall = true;
-    user = config.user.name;
+    #user = config.user.name;
+    settings =
+      let
+        inherit (config.services) mediaStore;
+        yes = "yes";
+      in
+      {
+        media_dir = [
+          "V,${mediaStore}/Videos/Movies"
+          "V,${mediaStore}/Videos/Cartoon_Movies"
+          "V,${mediaStore}/Videos/TV"
+          "V,${mediaStore}/Videos/Cartoon_Series"
+          "A,${mediaStore}/Music"
+          "P,${mediaStore}/Photos"
+        ];
+        inotify = yes;
+        wide_links = yes;
+      };
   };
-  environment.systemPackages = with pkgs; [
-    jellyfin
-    jellyfin-web
-    jellyfin-ffmpeg
-  ];
 }
