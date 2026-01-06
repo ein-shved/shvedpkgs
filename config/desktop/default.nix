dbbd36d652e607df71a57a88a0af7890ab83ae7d
Yury Shvedov <mestofel13@gmail.com>
2026-01-06 11:29:36 +0300

    desktop: move pointer and gtk config from hyprland to generic

--- config/desktop/default.nix
+++ config/desktop/default.nix
@@ -40,5 +40,33 @@ in
     environment.graphicPackages = with pkgs; [
       networkmanagerapplet
     ];
+    home = {
+      home.pointerCursor = {
+        gtk.enable = true;
+        # x11.enable = true;
+        package = pkgs.bibata-cursors;
+        name = "Bibata-Modern-Classic";
+        size = 16;
+      };
+
+      gtk = {
+        enable = true;
+
+        theme = {
+          package = pkgs.flat-remix-gtk;
+          name = "Flat-Remix-GTK-Grey-Darkest";
+        };
+
+        iconTheme = {
+          package = pkgs.adwaita-icon-theme;
+          name = "Adwaita";
+        };
+
+        font = {
+          name = "Sans";
+          size = 11;
+        };
+      };
+    };
   };
 }
--- config/desktop/hyprland/hyprland.nix
+++ config/desktop/hyprland/hyprland.nix
@@ -36,32 +36,6 @@ in
     ];
 
     home-manager.users.${user} = {
-      home.pointerCursor = {
-        gtk.enable = true;
-        # x11.enable = true;
-        package = pkgs.bibata-cursors;
-        name = "Bibata-Modern-Classic";
-        size = 16;
-      };
-
-      gtk = {
-        enable = true;
-
-        theme = {
-          package = pkgs.flat-remix-gtk;
-          name = "Flat-Remix-GTK-Grey-Darkest";
-        };
-
-        iconTheme = {
-          package = pkgs.adwaita-icon-theme;
-          name = "Adwaita";
-        };
-
-        font = {
-          name = "Sans";
-          size = 11;
-        };
-      };
       wayland.windowManager.hyprland = {
         enable = hyprlandEnabled;
         systemd.enable = true;
