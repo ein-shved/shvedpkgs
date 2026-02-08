746c28f7700bde5ff8860ee3cadb5867297c8e28
Yury Shvedov <mestofel13@gmail.com>
2026-02-08 16:13:22 +0300

    gerrit-admin-console-plugin: init

---
+++ pkgs/by-name/ge/gerrit-admin-console-plugin/package.nix
@@ -0,0 +1,7 @@
+{
+  fetchurl,
+}:
+fetchurl {
+  url = "https://gerrit-ci.gerritforge.com/job/plugin-admin-console-bazel-master-stable-3.13/lastSuccessfulBuild/artifact/bazel-bin/plugins/admin-console/admin-console.jar";
+  hash = "sha256-8piUXcmyIA4fpcIjwb7ZVdzN4AAzX667FCDhUR71VzM=";
+}
