324b41a4b3fcffeb6df23c3286c9cdb301c85cb5
Yury Shvedov <mestofel13@gmail.com>
2026-05-30 16:05:33 +0300

    gerrit-oauth-plugin: update to 3.14

--- pkgs/by-name/ge/gerrit-oauth-plugin/package.nix
+++ pkgs/by-name/ge/gerrit-oauth-plugin/package.nix
@@ -5,8 +5,8 @@
 runCommand "oauth.jar"
   {
     src = fetchurl {
-      url = "https://gerrit-ci.gerritforge.com/view/Plugins-stable-3.13/job/plugin-oauth-bazel-master-stable-3.13/4/artifact/bazel-bin/plugins/oauth/oauth.jar";
-      hash = "sha256-SopXmT8zN3JBUtcxWq3EqIzuTP1QxxF7Ed0boFGT5Uw=";
+      url = "https://gerrit-ci.gerritforge.com/job/plugin-oauth-bazel-stable-3.14/3/artifact/bazel-bin/plugins/oauth/oauth.jar";
+      hash = "sha256-Kc59bxE7gsy93RR+g8DD4iezFa412KO8tV14tDqkU+8=";
     };
   }
   ''
