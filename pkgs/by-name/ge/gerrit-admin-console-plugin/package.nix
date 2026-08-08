33914d442964457c9f22d1bebb7e1a7505f75dab
Yury Shvedov <mestofel13@gmail.com>
2026-08-08 13:49:43 +0300

    gerrit-admin-console-plugin: bump version

--- pkgs/by-name/ge/gerrit-admin-console-plugin/package.nix
+++ pkgs/by-name/ge/gerrit-admin-console-plugin/package.nix
@@ -3,5 +3,5 @@
 }:
 fetchurl {
   url = "https://gerrit-ci.gerritforge.com/job/plugin-admin-console-bazel-master-stable-3.13/lastSuccessfulBuild/artifact/bazel-bin/plugins/admin-console/admin-console.jar";
-  hash = "sha256-8piUXcmyIA4fpcIjwb7ZVdzN4AAzX667FCDhUR71VzM=";
+  hash = "sha256-zXJqFwbftm4rrX6Ibezi98xh7HlfOJNT1Xng3GfB9cQ=";
 }
